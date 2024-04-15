67a2b37c17a369dea23785e5eabe9d17817b1627
vrao5048 <vrao5048@uni.sydney.edu.au>
2024-04-16 02:46:58 +1000

    Graph displaying!

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    final node1 = Node.Id(1);
+    final node2 = Node.Id(2);
+    final node3 = Node.Id(3);
+    final node4 = Node.Id(4);
+    final node5 = Node.Id(5);
+    final node6 = Node.Id(6);
+    final node8 = Node.Id(7);
+    final node7 = Node.Id(8);
+    final node9 = Node.Id(9);
+    final node10 = Node.Id(10);
+    final node11 = Node.Id(11);
+    final node12 = Node.Id(12);
+
+    graph.addEdge(node1, node2);
+    graph.addEdge(node1, node3, paint: Paint()..color = Colors.red);
+    graph.addEdge(node1, node4, paint: Paint()..color = Colors.blue);
+    graph.addEdge(node2, node5);
+    graph.addEdge(node2, node6);
+    graph.addEdge(node6, node7, paint: Paint()..color = Colors.red);
+    graph.addEdge(node6, node8, paint: Paint()..color = Colors.red);
+    graph.addEdge(node4, node9);
+    graph.addEdge(node4, node10, paint: Paint()..color = Colors.black);
+    graph.addEdge(node4, node11, paint: Paint()..color = Colors.red);
+    graph.addEdge(node11, node12);
+
+    builder
+      ..siblingSeparation = (100)
+      ..levelSeparation = (150)
+      ..subtreeSeparation = (150)
+      ..orientation = (BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM);
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -107,33 +139,95 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           //   Create a container for the graph
-            Container(
+            SizedBox(
+              width: 390,
+              height: 720,
               //   This container will hold the graph and interactive viewer
-              margin: const EdgeInsets.all(10.0),
-              child: Wrap(
+              child: Column(
                 children: [
-                  Container(
-                    // Container of max width
-                    width: 100,
-                    // Create a text form to be filled
-                    child: TextFormField(
-                      initialValue: builder.siblingSeparation.toString(),
-                      decoration: const InputDecoration(labelText: "Sibling Separation"),
-                      onChanged: (text) {
-                        builder.siblingSeparation = int.tryParse(text) ?? 100;
-                        this.setState(() {});
-                      },
-                    ),
+                  Wrap(
+                    children: [
+                      Container(
+                        // Container of max width
+                        width: 100,
+                        // Create a text form to be filled
+                        child: TextFormField(
+                          initialValue: builder.siblingSeparation.toString(),
+                          decoration: const InputDecoration(labelText: "Sibling Separation"),
+                          onChanged: (text) {
+                            builder.siblingSeparation = int.tryParse(text) ?? 100;
+                            this.setState(() {});
+                          },
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        child: TextFormField(
+                          initialValue: builder.levelSeparation.toString(),
+                          decoration: const InputDecoration(labelText: "Level Separation"),
+                          onChanged: (text) {
+                            builder.levelSeparation = int.tryParse(text) ?? 100;
+                            this.setState(() {});
+                          },
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        child: TextFormField(
+                          initialValue: builder.subtreeSeparation.toString(),
+                          decoration: const InputDecoration(labelText: "SubTree Separation"),
+                          onChanged: (text) {
+                            builder.subtreeSeparation = int.tryParse(text) ?? 100;
+                            this.setState(() {});
+                          },
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        child: TextFormField(
+                          initialValue: builder.orientation.toString(),
+                          decoration: const InputDecoration(labelText: "Orientation"),
+                          onChanged: (text) {
+                            builder.orientation = int.tryParse(text) ?? 100;
+                            this.setState(() {});
+                          },
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          final node12 = Node.Id(r.nextInt(100));
+                          var edge = graph.getNodeAtPosition(r.nextInt(graph.nodeCount()));
+                          print(edge);
+                          graph.addEdge(edge, node12);
+                          setState(() {});
+                        },
+                        child: const Text("Add"),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 100,
-                    child: TextFormField(
-                      initialValue: builder.levelSeparation.toString(),
-                      decoration: const InputDecoration(labelText: "Level Separation"),
+                  Expanded(
+                    child: InteractiveViewer(
+                      constrained: false,
+                      boundaryMargin: EdgeInsets.all(100),
+                      minScale: 0.01,
+                      maxScale: 5.6,
+                      child: GraphView(
+                        graph: graph,
+                        algorithm: BuchheimWalkerAlgorithm(builder, TreeEdgeRenderer(builder)),
+                        paint: Paint()
+                          ..color = Colors.green
+                          ..strokeWidth = 1
+                          ..style = PaintingStyle.stroke,
+                        builder: (Node node) {
+                          // The type of widget shown can be decided to be changed here
+                          var a = node.key?.value as int;
+                          return rectangleWidget(a);
+                        },
+                      ),
                     ),
                   ),
                 ],
-              ),
+              )
             ),
           ],
         ),
@@ -162,6 +256,5 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final Graph graph = Graph()..isTree = true;
   BuchheimWalkerConfiguration builder = BuchheimWalkerConfiguration();
-
 
 }
